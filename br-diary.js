4aabd1cdb3ebb0834ed9cef4b4698756f9fcd564
kennethnorman <norman.kenneth@gmail.com>
2015-01-25 10:52:26 +0000

    Fix calendar and image clicker

--- br-diary.js
+++ br-diary.js
@@ -13,8 +13,28 @@ pagemessage2.innerText = "Working...";
 
 
 
-$(document).ready(UpdateCalendarEvents("https://www.google.com/calendar/feeds/af9f3hobdapolfftcs4o2usr34%40group.calendar.google.com/public/full?singleevents=true&futureevents=true&orderby=starttime&sortorder=ascending&alt=json"));
+$(document).ready(UpdateCalendarEvents(
+//"http://www.google.com/calendar/feeds/af9f3hobdapolfftcs4o2usr34%40group.calendar.google.com/public/full?singleevents=true&futureevents=true&orderby=starttime&sortorder=ascending&alt=json"
+"https://www.google.com/calendar/feeds/af9f3hobdapolfftcs4o2usr34%40group.calendar.google.com/public/basic?singleevents=true&futureevents=true&orderby=starttime&sortorder=ascending&alt=json"
+));
 
+var d = new Date();
+Math.seedrandom(d.getTime());
+
+var bannerpic = document.getElementById("tc-headerlogo");
+var bannerimage = document.getElementById("tc-headerimage");
+bannerpic.onclick = function(){
+	var r= Math.random();
+	var numImages= 2;
+	var ri= ((r*10)%numImages)+1;
+	var randomIndex= Math.floor(ri);
+	switch (randomIndex) {
+	case 1: bannerimage.src = "brlogo2014.jpg";
+		break;
+	case 2: bannerimage.src = "br1.png";
+		break;
+	}
+}
 
 /**/
 function UpdateCalendarEvents(sourceFile) {
@@ -32,7 +52,17 @@ function UpdateCalendarEvents(sourceFile) {
 
             var entry = $(el),
                 title = entry[0].title.$t,
-                when = new Date(entry[0].gd$when[0].startTime);
+                summary = entry[0].summary.$t;
+			var when = new Date("1990-01-01");
+			
+			// Format is "When: Sat 12 Sep 2015<br>\n\n\n<br>Event Status: confirmed"
+			var sum = summary.split("<br>");
+			for (var sumIndex= 0; sumIndex<sum.length; sumIndex++) {
+				var entry= sum[sumIndex].split(":");
+				if (entry[0] == "When") {
+					when = new Date(entry[1].trim());
+				}
+			}
 
             foundevents = true;
 
